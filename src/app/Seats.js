d861cadda54b2d98828013fa1eeb2a0f6fcbfc6f
Wargen Guittap <48695406+guittap@users.noreply.github.com>
2023-05-18 18:02:33 -0700

    finished first page

---
+++ src/app/Seats.js
@@ -0,0 +1,23 @@
+export default function Seats(props) {
+  return (
+    <div className="howMany flex justify-center items-center grow ">
+      <div className="flex flex-col justify-center">
+        <div className="flex justify-center">
+          <h1 className="text-8xl">{props.seatAmount}</h1>
+          <div className="flex flex-col ml-7">
+            <button className="plusButton text-5xl" onClick={props.addSeat}>
+              +
+            </button>
+            <button
+              className="minusButton text-5xl"
+              onClick={props.subtractSeat}
+            >
+              -
+            </button>
+          </div>
+        </div>
+        <h2 className="text-2xl">how many?</h2>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,39 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import Seats from "./Seats";
 
 export default function Home() {
-  let seatAmount = 2,
-    subtotal,
-    tax,
-    taxPercentage,
-    tip,
-    tipPercentage,
-    grandTotal;
+  const [seatAmount, setSeatAmount] = useState(2);
+  const [subtotal, setSubtotal] = useState(0);
+  const [tax, setTax] = useState(0);
+  const [taxPercentage, setTaxPercentage] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [tipPercentage, setTipPercentage] = useState(0);
+  const [grandTotal, setGrandTotal] = useState(0);
 
   function addSeat() {
-    seatAmount++;
+    if (seatAmount < 9) setSeatAmount(seatAmount + 1);
   }
+
+  function subtractSeat() {
+    if (seatAmount > 2) setSeatAmount(seatAmount - 1);
+  }
+
+  function toSubtotalPage() {}
 
   return (
     <main className="bg-slate-300 h-screen flex justify-center items-center">
-      <div className="max-w-md h-screen bg-white overflow-hidden">
-        <div className="actionScreen w-screen h-2/3"></div>
-        <div className="interactionScreen w-screen h-1/3 bg-primary text-white overflow-hidden">
-          <div className="howMany relative w-screen">
-            <div className="absolute w-max top-12 left-1/2 transform -translate-x-1/2 ">
-              <div className="flex relative mb-4">
-                <h1 className="text-8xl">{seatAmount}</h1>
-                <div className="flex-none">
-                  <button className="plusButton text-5xl" onClick={addSeat}>
-                    +
-                  </button>
-                  <button className="minusButton text-5xl">-</button>
-                </div>
-              </div>
-              <h2 className="text-2xl">how many?</h2>
-            </div>
-          </div>
+      <div className="max-w-md h-screen bg-white overflow-hidden flex flex-col grow">
+        <div className="actionScreen h-2/3 overflow-hidden flex items-end justify-end">
+          <button className="text-8xl mr-3">→</button>
+        </div>
+        <div className="interactionScreen h-1/3 bg-primary text-white overflow-hidden flex">
+          <Seats
+            seatAmount={seatAmount}
+            addSeat={addSeat}
+            subtractSeat={subtractSeat}
+          />
         </div>
       </div>
     </main>
