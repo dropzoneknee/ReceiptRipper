9300db285f94ff946a978609ca4fd95a9c7bcc2f
Wargen Guittap <48695406+guittap@users.noreply.github.com>
2023-05-21 19:58:42 -0700

    Revert "pwa application"
    This reverts commit e49dc500a78e5726cd48da1488806bc74c0a099d.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  // disable: process.env.NODE_ENV === 'development',
-  // register: true,
-  scope: "/app",
-  // sw: 'service-worker.js',
-  //...
-});
+const nextConfig = {}
 
-module.exports = withPWA({
-  // next.js config
-});
+module.exports = nextConfig
